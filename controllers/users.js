dbac1c72efeab2f7d8ddc2fd00d4c9d60a71faea
Kishan <kpate262@uic.edu>
2018-08-10 07:44:08 -0500

    add users.pug to views and modified users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,5 +1,5 @@
 module.exports.controller = (app) => {
     app.get('/users', (req, res) => {
-        res.render('index', {title: 'Users'});
+        res.render('users', {title: 'Users', description: 'This is the description of all the users'}); //this uses Users.pug file to render
     })
 }
